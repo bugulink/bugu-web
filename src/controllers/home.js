4d191dec95fef13d155f7badf25594697f18bc0a
helloyou2012 <helloyou2012@yeah.net>
2018-01-29 20:18:23 +0800

    wait 1min email

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -7,6 +7,17 @@ export async function main(ctx) {
 export async function captcha(ctx) {
   const { User } = ctx.orm();
   const { email } = ctx.request.body;
+  const { lastTime } = ctx.session;
+
+  // wait 1 minute
+  const min = 60 * 1000;
+  const now = Date.now();
+  if (lastTime && (now - lastTime) <= min) {
+    ctx.throw(400, 'Try again in a minute');
+  } else {
+    ctx.session.lastTime = now;
+  }
+
   let user = await User.findOne({
     where: { email }
   });
